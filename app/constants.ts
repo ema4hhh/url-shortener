270d5cf8794d1c230e53dd6157ea0e98b5ec3b91
Emanuel <ema4hhh@gmail.com>
2024-05-21 16:44:17 -0500

    added create page and short path generator

---
+++ app/constants.ts
@@ -0,0 +1,2 @@
+export const BASE_62_CHAR_SET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+export const SHORT_LINK_LENGTH = 7
---
+++ app/create/_lib/generateShortUrl.ts
@@ -0,0 +1,13 @@
+'use server'
+
+import { BASE_62_CHAR_SET, SHORT_LINK_LENGTH } from "@/app/constants"
+
+export const generateShortUrl = () => {
+  let shortenedUrl: string[] = new Array(SHORT_LINK_LENGTH)
+  for (let i = 0; i < SHORT_LINK_LENGTH; i++) {
+    const randomIndex = Math.floor(Math.random() * BASE_62_CHAR_SET.length)
+    shortenedUrl[i] = BASE_62_CHAR_SET[randomIndex]
+  }
+
+  return shortenedUrl.join("")
+}
