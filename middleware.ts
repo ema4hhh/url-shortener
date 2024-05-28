0220872fcf33cf67f09ac0dbf4e05d897455dc65
Emanuel <ema4hhh@gmail.com>
2024-05-28 14:42:06 -0500

    redirect function added

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,27 @@
-import { type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 import { updateSession } from './utils/supabase/middleware'
+import { createClient } from './utils/supabase/server'
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  if (request.nextUrl.pathname.startsWith('/app/create')) {
+    return await updateSession(request)
+  }
+
+  const shortUrl = request.nextUrl.pathname.split('/', 2)[1]
+
+  if (shortUrl !== undefined && shortUrl !== 'app') {
+    const supabase = createClient();
+
+    const longUrl = await supabase.from('urls').select('long_url').eq('short_url', shortUrl).single()
+
+    if (longUrl.data !== null) {
+      return NextResponse.redirect(longUrl.data.long_url)
+    }
+  }
 }
 
 export const config = {
   matcher: [
-    '/create',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 }
