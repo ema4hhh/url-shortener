91298c8d7ca6dffc58157703be9d745e2e55357c
Emanuel <ema4hhh@gmail.com>
2024-05-30 17:12:11 -0500

    basic error handling storing urls and ensure unique url is generated

--- app/app/create/_lib/storeNewUrl.ts
+++ app/app/create/_lib/storeNewUrl.ts
@@ -6,13 +6,41 @@ import { generateShortUrl } from "./generateShortUrl";
 export const storeNewUrl = async (url: string, username: string) => {
   const supabase = createClient();
   const lowerCaseUsername = username.toLocaleLowerCase()
+  let newShortUrl: string;
+  let errorMessage = '';
+
+  // while the generated short url is already in use, generate a new one
+  while (true) {
+    let iterations = 0;
+    newShortUrl = generateShortUrl();
+    const { data } = await supabase.from("urls").select().eq('short_url', newShortUrl);
+
+    if (iterations > 10) {
+      errorMessage = "I am sorry, there is too many generated links already, please contact me in github";
+      break;
+    };
+
+    if (data === null) {
+      errorMessage = "I am sorry, there is an error with the database, please contact me in github";
+      break;
+    };
+
+    if (data.length === 0) break;
+
+    iterations++
+  }
+
+  if (errorMessage !== '') {
+    throw new Error(errorMessage);
+  };
 
   await supabase.from("urls").insert([
     {
-      id: 3,
       username: lowerCaseUsername,
       long_url: url,
-      short_url: generateShortUrl(),
+      short_url: newShortUrl,
     }
   ]);
+
+  return newShortUrl;
 }
--- app/app/create/_ui/Form.tsx
+++ app/app/create/_ui/Form.tsx
@@ -1,8 +1,11 @@
 "use client"
 
+import { useState } from "react";
 import { storeNewUrl } from "../_lib/storeNewUrl";
 
 function Form() {
+  const [errorMessage, setErrorMessage] = useState<string>()
+  const [newGeneratedUrl, setNewGeneratedUrl] = useState<string>()
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -12,21 +15,38 @@ function Form() {
     const url = formData.get('url') as string | null
     const username = formData.get('username')?.toString() ?? 'admin'
 
-    url && storeNewUrl(url, username)
+    if (url) {
+      storeNewUrl(url, username)
+        .then(newUrl => {
+          setNewGeneratedUrl(newUrl)
+          setErrorMessage('')
+        })
+        .catch(err => {
+          console.error(err)
+          setErrorMessage(err.message)
+        })
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        <span>Username</span>
-        <input name="username" type="text" placeholder="john doe" className="text-slate-900" />
-      </label>
-      <label>
-        <span>URL</span>
-        <input name="url" type="text" placeholder="longurl.com/lorem/ipsum" className="text-slate-900" />
-      </label>
-      <button type="submit">Shorten</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Username</span>
+          <input name="username" type="text" placeholder="john doe" className="text-slate-900" />
+        </label>
+        <label>
+          <span>URL</span>
+          <input name="url" type="text" placeholder="longurl.com/lorem/ipsum" className="text-slate-900" />
+        </label>
+        <button type="submit">Shorten</button>
+        {
+          newGeneratedUrl && <>Short URL: <output>{newGeneratedUrl}</output></>
+        }
+      </form>
+
+      {errorMessage && <p>Something went wrong: {errorMessage}</p>}
+    </>
   )
 }
 
