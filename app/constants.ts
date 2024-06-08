3fdf7436be643dc0ead3e9e6d561622e62e1934f
Emanuel <ema4hhh@gmail.com>
2024-06-08 13:01:44 -0500

    moved pages links to constants

--- app/constants.ts
+++ app/constants.ts
@@ -1,2 +1,21 @@
 export const BASE_62_CHAR_SET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 export const SHORT_LINK_LENGTH = 7
+
+export const pageLinks = [
+  {
+    name: 'Home',
+    href: '/'
+  },
+  {
+    name: 'App Home',
+    href: '/app'
+  },
+  {
+    name: 'Create',
+    href: '/app/create'
+  },
+  {
+    name: 'Urls List',
+    href: '/app/urls'
+  }
+]
