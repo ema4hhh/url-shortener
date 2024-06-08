e2e2124d1d2aa2c47652237e52b4491348ebd800
Emanuel <ema4hhh@gmail.com>
2024-06-08 13:02:25 -0500

    "/" page improved and styled

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <button><Link href="/app/create">Go to create</Link></button>
+    <main className="gap-6">
+      <h1>Welcome to the URL shortener service</h1>
+      <h4>You can go ahead and <Link className="link" href="/app/urls">see the list of links</Link> or <Link className="link" href="/app/create">create a short URL</Link></h4>
     </main>
   );
 }
