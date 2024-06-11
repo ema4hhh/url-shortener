4b074529c0f748a0c8478ba130712cff3c67e31a
Emanuel <ema4hhh@gmail.com>
2024-06-11 16:58:10 -0500

    fixed mobile unnecessary scroll

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -2,11 +2,11 @@ import SideNav from "./_ui/SideNav"
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    <div className="flex min-h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64 bg-slate-200">
         <SideNav />
       </div>
-      <div className="flex flex-grow md:overflow-y-auto justify-center items-center">{children}</div>
+      <div className="flex flex-grow md:overflow-y-auto justify-center">{children}</div>
     </div>
   )
 }
