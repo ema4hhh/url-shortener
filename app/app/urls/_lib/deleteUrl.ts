d6cf79685db1e25cf619861477793d8eeaced49d
Emanuel <ema4hhh@gmail.com>
2024-06-12 12:29:37 -0500

    fixed not deleting url bug

--- app/app/urls/_lib/deleteUrl.ts
+++ app/app/urls/_lib/deleteUrl.ts
@@ -4,7 +4,15 @@ import { createClient } from "@/utils/supabase/server"
 
 export const deleteUrl = async(shortUrl: string) => {
   const supabase = createClient()
-  const { error } = await supabase.from('urls').delete().eq('short_url', shortUrl)
+
+  const databaseShortUrlFormat = shortUrl.split('/').pop()
+
+  console.log(databaseShortUrlFormat);
+  
+
+  if (!databaseShortUrlFormat) return
+
+  const { error } = await supabase.from('urls').delete().eq('short_url', databaseShortUrlFormat)
 
   return error
 }
